fff0aab8eb1a1e0ea35f7e069e31c487bbe85aeb
ankushmishra1 <mishraankush405@gmail.com>
2024-10-29 17:11:04 +0530

    CalculaSqu

---
+++ CalculaSquare.java
@@ -0,0 +1,28 @@
+package com.labs;
+
+public class CalculaSquare {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Initialize the length of the side of the square
+		float side = 3.2f;
+
+		// Calculate the area of the square (side * side)
+		float area = side * side;
+
+		// Calculate the perimeter of the square (4 * side)
+		float perimeter = 4 * side;
+
+		// Display Area of the square
+		System.out.println("Area of the square: " + area);
+
+		// Display Perimeter of the square
+		System.out.println("Perimeter of the square: " + perimeter);
+
+	}
+}
+
+/*
+ * Area of the square: 10.240001
+ *  Perimeter of the square: 12.8
+ */
